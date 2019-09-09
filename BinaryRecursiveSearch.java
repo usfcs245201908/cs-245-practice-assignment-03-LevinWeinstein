1586851e61fe146254dc262c1ea0df50ba0d3e0c
levinweinstein <lweinstein@gomerits.com>
2019-09-08 20:54:41 -0700

    Added search options. Have not compared it with spec yet to lol. So I should do that at some point.

---
+++ BinaryRecursiveSearch.java
@@ -0,0 +1,21 @@
+public class BinaryRecursiveSearch implements Practice03Search {
+    public String searchName(){
+        return "Binary Recursive Search";
+    }
+
+    public int search(int [] arr, int target){
+        return search(arr, target, 0, arr.length - 1);
+    }
+
+    public int search(int [] arr, int target, int low, int high){
+        if (low > high)
+            return -1;
+
+        int mid = (low + high) / 2;
+        if (arr[mid] > target)
+            return search(arr, target, low, mid - 1);
+        if (arr[mid] < target)
+            return search(arr, target, mid + 1, high);
+        return mid;
+    }
+}
